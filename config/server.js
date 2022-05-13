8a008fc6b140bcdaf42ef4f6334a7e14f37a4693
Katarzyna Stoklosa <katarzyna.a.stoklosa@gmail.com>
2022-05-13 22:40:22 +0200

    chore: init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ec9921f90676e5f2636e08060c1ad78a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'de1d05f6-aab8-43b5-af59-b893a72aeedd'
+};
